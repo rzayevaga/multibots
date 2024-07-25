18216d58f42f4221fcfe3e74e0b3bf89932d453d
Ağa Rzayev <rzayevaga.dev@gmail.com>
2024-07-25 11:26:01 +0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -16,7 +16,7 @@ for bot_name, bot_config in bots.items():
     for env_name, env_value in bot_config['env'].items():
         os.environ[env_name] = env_value
     
-    bot_dir = f"/Rzayev/{bot_name}"
+    bot_dir = f"/app/{bot_name}"
     requirements_file = os.path.join(bot_dir, 'requirements.txt')
     bot_file = os.path.join(bot_dir, bot_config['run'])
 
